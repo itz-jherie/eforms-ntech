cb910fc31900dc50c021c8b953346c286b080a52
Ayobami <aycodergymer21@gmail.com>
2023-01-19 11:53:39 +0100

    updated pages

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+<<<<<<< HEAD
 import Hero from "../../components/hero/Hero"
 import Main from "../../components/main/Main"
 import Maincontent from '../../components/maincontent/Maincontent';
+=======
+import Hero from "../components/hero/Hero"
+import Main from "../components/main/Main"
+import Maincontent from '../components/maincontent/Maincontent';
+>>>>>>> 89c4c84 (updated pages)
 
 const Home = () => {
   return (
