36162308e41b15ca61b4990a38ff2655bb498de4
Jeremiah Elegba <64599754+itz-jherie@users.noreply.github.com>
2023-01-20 10:52:29 +0100

    fixed layout

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import Footer from '../../components/footer/Footer';
 import Hero from "../../components/hero/Hero"
 import Main from "../../components/main/Main"
 import Maincontent from '../../components/maincontent/Maincontent';
+import data from '../../assets/dataFiles/footerData'
+import Nav from '../../components/nav/Nav';
 
 const Home = () => {
   return (
     <>
+     <Nav /> 
     <Hero />
     <Main />
     <Maincontent />
+    <Footer
+      logo={data.logo}
+      name={data.name}
+      link1={data.link1}
+      link2={data.link2}
+      link3={data.link3}
+      link4={data.link4}
+      term1={data.term1}
+      term2={data.term2}
+      term3={data.term3}
+      term4={data.term4}
+      contact1={data.contact1}
+      contact2={data.contact2}
+      copyright={data.copyright}
+    />
     </>
   )
 }
--- src/pages/layout/Layout.js
+++ src/pages/layout/Layout.js
@@ -7,27 +7,13 @@ import Nav from '../../components/nav/Nav'
 const Layout = () => {
   return (
     <>
-    {/* <Nav /> */}
+    
 
     <section>
       <Outlet />
     </section>
 
-    {/* <Footer
-      logo={data.logo}
-      name={data.name}
-      link1={data.link1}
-      link2={data.link2}
-      link3={data.link3}
-      link4={data.link4}
-      term1={data.term1}
-      term2={data.term2}
-      term3={data.term3}
-      term4={data.term4}
-      contact1={data.contact1}
-      contact2={data.contact2}
-      copyright={data.copyright}
-    /> */}
+ 
     </>
   )
 }
