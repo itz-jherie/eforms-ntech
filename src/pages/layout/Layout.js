84c957852dc49bf6bf85bb170ce4c7aac404a16c
Ayobami <aycodergymer21@gmail.com>
2023-01-23 13:34:14 +0100

    weekend changes by ay

--- src/pages/layout/Layout.js
+++ src/pages/layout/Layout.js
@@ -3,13 +3,14 @@ import {Outlet} from "react-router-dom"
 import data from '../../assets/dataFiles/footerData'
 import Footer from '../../components/footer/Footer'
 import Nav from '../../components/nav/Nav'
+import App from "../../App.css"
 
 const Layout = () => {
   return (
     <>
     
 
-    <section>
+    <section >
       <Outlet />
     </section>
 
